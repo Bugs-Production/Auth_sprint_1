30a7cf33416e1ccbfa84a0942ae06e8569570d60
Natalia B <naffa.bob@gmail.com>
2024-08-27 14:50:38 +0500

    Добавить документацию для openapi

--- src/api/v1/users.py
+++ src/api/v1/users.py
@@ -13,7 +13,26 @@ from services.user import UserService, get_user_service
 router = APIRouter()
 
 
-@router.get("/{user_id}", response_model=UserSchema)
+@router.get(
+    "/{user_id}",
+    response_model=UserSchema,
+    summary="Информация о пользователе",
+    response_description="Полная информация о пользователе",
+    responses={
+        HTTPStatus.NOT_FOUND: {
+            "description": "Пользователь не найден",
+            "content": {"application/json": {"example": {"detail": "user not found"}}},
+        },
+        HTTPStatus.UNAUTHORIZED: {
+            "description": "Ошибка валидации токена",
+            "content": {"application/json": {"example": {"detail": "invalid token"}}},
+        },
+        HTTPStatus.FORBIDDEN: {
+            "description": "Доступ запрещен",
+            "content": {"application/json": {"example": {"detail": "Forbidden"}}},
+        },
+    },
+)
 async def get_user_info(
     user_id: UUID,
     auth_data: dict[str, Any] = Depends(authenticate_user),
@@ -29,7 +48,26 @@ async def get_user_info(
     return user
 
 
-@router.get("/{user_id}/login_history", response_model=Page[UserLoginHistorySchema])
+@router.get(
+    "/{user_id}/login_history",
+    response_model=Page[UserLoginHistorySchema],
+    summary="Информация об истории логинов пользователя",
+    response_description="Список всех логинов пользователя с пагинацией",
+    responses={
+        HTTPStatus.NOT_FOUND: {
+            "description": "Пользователь не найден",
+            "content": {"application/json": {"example": {"detail": "user not found"}}},
+        },
+        HTTPStatus.UNAUTHORIZED: {
+            "description": "Ошибка валидации токена",
+            "content": {"application/json": {"example": {"detail": "invalid token"}}},
+        },
+        HTTPStatus.FORBIDDEN: {
+            "description": "Доступ запрещен",
+            "content": {"application/json": {"example": {"detail": "Forbidden"}}},
+        },
+    },
+)
 async def get_user_info(
     user_id: UUID,
     auth_data: dict[str, Any] = Depends(authenticate_user),
@@ -45,7 +83,34 @@ async def get_user_info(
     return paginate(user_history)
 
 
-@router.put("/{user_id}", response_model=UpdateUserSchema)
+@router.put(
+    "/{user_id}",
+    response_model=UpdateUserSchema,
+    summary="Изменения данных пользователя",
+    response_description="Актуальная информация о пользователе",
+    responses={
+        HTTPStatus.NOT_FOUND: {
+            "description": "Пользователь не найден",
+            "content": {"application/json": {"example": {"detail": "user not found"}}},
+        },
+        HTTPStatus.CONFLICT: {
+            "description": "Ошибка при обновлении данных",
+            "content": {
+                "application/json": {
+                    "example": {"detail": "user with this parameters already exists"}
+                }
+            },
+        },
+        HTTPStatus.UNAUTHORIZED: {
+            "description": "Ошибка валидации токена",
+            "content": {"application/json": {"example": {"detail": "invalid token"}}},
+        },
+        HTTPStatus.FORBIDDEN: {
+            "description": "Доступ запрещен",
+            "content": {"application/json": {"example": {"detail": "Forbidden"}}},
+        },
+    },
+)
 async def put_user_info(
     user_id: UUID,
     request_data: UpdateUserSchema,
