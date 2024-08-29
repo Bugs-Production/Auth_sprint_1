a2e9c0a6c49a868f37abe69fff1757e6ed98ffce
Natalia B <naffa.bob@gmail.com>
2024-08-29 10:11:26 +0500

    Удалены лишние модели из схем

--- src/schemas/auths.py
+++ src/schemas/auths.py
@@ -1,17 +1,4 @@
-from uuid import UUID
-
 from pydantic import BaseModel
-
-
-class AccessToken(BaseModel):
-    user_id: UUID
-    exp: int
-    roles: list[str]
-
-
-class RefreshToken(BaseModel):
-    user_id: UUID
-    exp: int
 
 
 class AuthOutputSchema(BaseModel):
