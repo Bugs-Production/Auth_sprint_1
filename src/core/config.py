a9d43d3560b35437ff49eec76d407507411c842b
Natalia B <naffa.bob@gmail.com>
2024-09-02 18:18:59 -0500

    Переформатирован класс настроек Settings. Обновлен env.example

--- src/core/config.py
+++ src/core/config.py
@@ -5,24 +5,22 @@ JWT_ALGORITHM = "HS256"
 
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(
-        env_file=".env",
-        env_file_encoding="utf-8",
-        extra="ignore",
-    )
-    project_name: str = Field("Users", alias="PROJECT_NAME")
-    postgres_user: str = Field("app", alias="POSTGRES_USER")
-    postgres_db: str = Field("database", alias="POSTGRES_DB")
+    model_config = SettingsConfigDict(extra="ignore")
+
+    ENVIRONMENT: str = ENV_PRODUCTION
+
+    project_name: str = "Auth service"
+
+    postgres_user: str = Field("postgres", alias="POSTGRES_USER")
     postgres_password: str = Field("postgres", alias="POSTGRES_PASSWORD")
-    postgres_host: str = Field("postgres", alias="POSTGRES_HOST")
+    postgres_host: str = Field("db", alias="POSTGRES_HOST")
     postgres_port: int = Field(5432, alias="POSTGRES_PORT")
+    postgres_db: str = Field("foo", alias="POSTGRES_DB")
     postgres_url: str = Field(
         "postgresql+asyncpg://postgres:postgres@db:5432/foo", alias="POSTGRES_URL"
     )
-    test_postgres_url: str = Field(
-        "postgresql+asyncpg://postgres:postgres@db:5432/test_foo",
-        alias="TEST_POSTGRES_URL",
-    )
+    sql_echo: bool = Field(False, alias="SQL_ECHO")
+
     redis_host: str = Field("127.0.0.1", alias="REDIS_HOST")
     redis_port: int = Field(6379, alias="REDIS_PORT")
 
