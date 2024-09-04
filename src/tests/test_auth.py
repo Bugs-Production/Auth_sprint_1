ae1515c2bf387aeb34daf7b8e19393dedac17a66
Natalia B <naffa.bob@gmail.com>
2024-09-05 02:34:57 +0300

    Исправлены тесты для auth

--- src/tests/test_auth.py
+++ src/tests/test_auth.py
@@ -5,7 +5,8 @@ import pytest
 from fastapi import status
 
 from core.config import JWT_ALGORITHM
-from tests.conftest import client
+from tests.conftest import (access_token_moderator, client, create_admin,
+                            create_moderator, refresh_token_moderator)
 
 endpoint = "/api/v1/auth"
 
@@ -29,62 +30,62 @@ def _signup_user(user_data):
             status.HTTP_200_OK,
             ("access_token", "refresh_token"),
         ),
-        # (
-        #         {
-        #             "password": "pass1",
-        #             "first_name": "user1_first_name",
-        #             "last_name": "user1_last_name",
-        #         },
-        #         status.HTTP_422_UNPROCESSABLE_ENTITY,
-        #         (),
-        # ),
-        # (
-        #         {
-        #             "login": "user1",
-        #             "first_name": "user1_first_name",
-        #             "last_name": "user1_last_name",
-        #         },
-        #         status.HTTP_422_UNPROCESSABLE_ENTITY,
-        #         (),
-        # ),
-        # (
-        #         {
-        #             "login": "",
-        #             "password": "pass1",
-        #             "first_name": "user1_first_name",
-        #             "last_name": "user1_last_name",
-        #         },
-        #         status.HTTP_422_UNPROCESSABLE_ENTITY,
-        #         (),
-        # ),
-        # (
-        #         {
-        #             "login": "user1",
-        #             "password": "",
-        #             "first_name": "user1_first_name",
-        #             "last_name": "user1_last_name",
-        #         },
-        #         status.HTTP_422_UNPROCESSABLE_ENTITY,
-        #         (),
-        # ),
-        # (
-        #         {
-        #             "login": "user1",
-        #             "password": "pass1",
-        #             "first_name": "",
-        #             "last_name": "",
-        #         },
-        #         status.HTTP_200_OK,
-        #         ("access_token", "refresh_token"),
-        # ),
-        # (
-        #         {
-        #             "login": "user1",
-        #             "password": "pass1",
-        #         },
-        #         status.HTTP_200_OK,
-        #         ("access_token", "refresh_token"),
-        # ),
+        (
+            {
+                "password": "pass1",
+                "first_name": "user1_first_name",
+                "last_name": "user1_last_name",
+            },
+            status.HTTP_422_UNPROCESSABLE_ENTITY,
+            (),
+        ),
+        (
+            {
+                "login": "user1",
+                "first_name": "user1_first_name",
+                "last_name": "user1_last_name",
+            },
+            status.HTTP_422_UNPROCESSABLE_ENTITY,
+            (),
+        ),
+        (
+            {
+                "login": "",
+                "password": "pass1",
+                "first_name": "user1_first_name",
+                "last_name": "user1_last_name",
+            },
+            status.HTTP_422_UNPROCESSABLE_ENTITY,
+            (),
+        ),
+        (
+            {
+                "login": "user1",
+                "password": "",
+                "first_name": "user1_first_name",
+                "last_name": "user1_last_name",
+            },
+            status.HTTP_422_UNPROCESSABLE_ENTITY,
+            (),
+        ),
+        (
+            {
+                "login": "user1",
+                "password": "pass1",
+                "first_name": "",
+                "last_name": "",
+            },
+            status.HTTP_200_OK,
+            ("access_token", "refresh_token"),
+        ),
+        (
+            {
+                "login": "user1",
+                "password": "pass1",
+            },
+            status.HTTP_200_OK,
+            ("access_token", "refresh_token"),
+        ),
     ],
 )
 @pytest.mark.asyncio
@@ -97,7 +98,7 @@ async def test_signup(user_data, expected_status, expected_fields):
     response_data = response.json()
     for field in expected_fields:
         assert field in response_data
-    sleep(5)
+
 
 @pytest.mark.asyncio
 async def test_login_success():
@@ -146,147 +147,125 @@ async def test_login_success():
     assert login_history_response.status_code == status.HTTP_200_OK
 
 
-# @pytest.mark.parametrize(
-#     "user_data, expected_status, expected_fields",
-#     [
-#         # (
-#         #         {
-#         #             "login": "login_user1",
-#         #             "password": "wrong_admin_password",
-#         #         },
-#         #         status.HTTP_400_BAD_REQUEST,
-#         #         (),
-#         # ),
-#         # (
-#         #         {
-#         #             "login": "unknown_user",
-#         #             "password": "unknown_pass",
-#         #
-#         #         },
-#         #         status.HTTP_404_NOT_FOUND,
-#         #         (),
-#         # ),
-#         # (
-#         #         {
-#         #             "password": "password",
-#         #         },
-#         #         status.HTTP_422_UNPROCESSABLE_ENTITY,
-#         #         (),
-#         # ),
-#         # (
-#         #         {
-#         #             "login": "",
-#         #             "password": "password",
-#         #
-#         #         },
-#         #         status.HTTP_422_UNPROCESSABLE_ENTITY,
-#         #         (),
-#         # ),
-#         # (
-#         #         {
-#         #             "login": "unknown_user",
-#         #         },
-#         #         status.HTTP_422_UNPROCESSABLE_ENTITY,
-#         #         (),
-#         # ),
-#         (
-#             {
-#                 "login": "unknown_user",
-#                 "password": "",
-#             },
-#             status.HTTP_422_UNPROCESSABLE_ENTITY,
-#             (),
-#         ),
-#     ],
-# )
-# @pytest.mark.asyncio
-# async def test_login_failed(user_data, expected_status, expected_fields):
-#     url = f"{endpoint}/login"
-#     response = client.post(url=url, json=user_data)
-#
-#     assert response.status_code == expected_status
-#
-#     response_data = response.json()
-#     for field in expected_fields:
-#         assert field in response_data
-#
+@pytest.mark.parametrize(
+    "user_data, expected_status, expected_fields",
+    [
+        (
+            # Пользователь есть, но ввел неправильный пароль
+            {
+                "login": "admin_user",
+                "password": "wrong_admin_password",
+            },
+            status.HTTP_400_BAD_REQUEST,
+            (),
+        ),
+        (
+            # Пользователя нет в БД
+            {
+                "login": "unknown_user",
+                "password": "unknown_pass",
+            },
+            status.HTTP_404_NOT_FOUND,
+            (),
+        ),
+        (
+            # Введены некорректные данные
+            {
+                "password": "password",
+            },
+            status.HTTP_422_UNPROCESSABLE_ENTITY,
+            (),
+        ),
+        (
+            # Введены некорректные данные
+            {
+                "login": "",
+                "password": "password",
+            },
+            status.HTTP_422_UNPROCESSABLE_ENTITY,
+            (),
+        ),
+        (
+            # Введены некорректные данные
+            {
+                "login": "unknown_user",
+            },
+            status.HTTP_422_UNPROCESSABLE_ENTITY,
+            (),
+        ),
+        (
+            # Введены некорректные данные
+            {
+                "login": "unknown_user",
+                "password": "",
+            },
+            status.HTTP_422_UNPROCESSABLE_ENTITY,
+            (),
+        ),
+    ],
+)
+@pytest.mark.asyncio
+async def test_login_failed(create_admin, user_data, expected_status, expected_fields):
+    url = f"{endpoint}/login"
+    response = client.post(url=url, json=user_data)
+
+    assert response.status_code == expected_status
+
+    response_data = response.json()
+    for field in expected_fields:
+        assert field in response_data
+
 
 @pytest.mark.asyncio
-async def test_logout():
-    access_token, refresh_token = _signup_user(
-        {
-            "login": "U1",
-            "password": "pass1",
-            "first_name": "U1",
-            "last_name": "U1",
-        },
-    )
+async def test_logout(
+    create_moderator, access_token_moderator, refresh_token_moderator
+):
     url = f"{endpoint}/logout"
 
-    wrong_access_token = access_token[::-1]
-    wrong_refresh_token = refresh_token[::-1]
+    # Попытка разлогиниться с неправильными токенами
+    wrong_access_token = access_token_moderator[::-1]
+    wrong_refresh_token = refresh_token_moderator[::-1]
     response1 = client.post(
         url,
         json={"access_token": wrong_access_token, "refresh_token": wrong_refresh_token},
     )
     assert response1.status_code == status.HTTP_401_UNAUTHORIZED
 
+    # Попытка разлогиниться с правильными токенами
     response2 = client.post(
-        url, json={"access_token": access_token, "refresh_token": refresh_token}
+        url,
+        json={
+            "access_token": access_token_moderator,
+            "refresh_token": refresh_token_moderator,
+        },
     )
     assert response2.status_code == status.HTTP_200_OK
-#
-#
-# @pytest.mark.asyncio
-# async def test_refresh():
-#     access_token, refresh_token = _signup_user(
-#         {
-#             "login": "U1",
-#             "password": "pass1",
-#             "first_name": "U1",
-#             "last_name": "U1",
-#         },
-#     )
-#
-#     payload = jwt.decode(
-#         access_token, options={"verify_signature": False}, algorithms=[JWT_ALGORITHM]
-#     )
-#
-#     user_history_url = f"/api/v1/users/{payload['user_id']}/login_history"
-#
-#     login_response = client.get(
-#         user_history_url,
-#         headers={
-#             "Authorization": f"Bearer {access_token}",
-#         },
-#     )
-#     assert login_response.status_code == status.HTTP_200_OK
-#
-#     url = f"{endpoint}/refresh"
-#
-#     wrong_access_token = access_token[::-1]
-#     wrong_refresh_token = refresh_token[::-1]
-#     response1 = client.post(
-#         url,
-#         json={"access_token": wrong_access_token, "refresh_token": wrong_refresh_token},
-#     )
-#     assert response1.status_code == status.HTTP_401_UNAUTHORIZED
-#
-#     response2 = client.post(
-#         url, json={"access_token": access_token, "refresh_token": refresh_token}
-#     )
-#     assert response2.status_code == status.HTTP_200_OK
-#
-#     response2_data = response2.json()
-#     assert "access_token" in response2_data
-#     assert "refresh_token" in response2_data
-#
-#     new_access_token = response2_data["access_token"]
-#
-#     login_response = client.get(
-#         user_history_url,
-#         headers={
-#             "Authorization": f"Bearer {new_access_token}",
-#         },
-#     )
-#     assert login_response.status_code == status.HTTP_200_OK
+
+
+@pytest.mark.asyncio
+async def test_refresh(
+    create_moderator, access_token_moderator, refresh_token_moderator
+):
+    refresh_url = f"{endpoint}/refresh"
+    # Запрос на обновление с неправильными токенами
+    wrong_access_token = access_token_moderator[::-1]
+    wrong_refresh_token = refresh_token_moderator[::-1]
+    response1 = client.post(
+        refresh_url,
+        json={"access_token": wrong_access_token, "refresh_token": wrong_refresh_token},
+    )
+    assert response1.status_code == status.HTTP_401_UNAUTHORIZED
+
+    # Запрос на обновление с правильными токенами
+    response2 = client.post(
+        refresh_url,
+        json={
+            "access_token": access_token_moderator,
+            "refresh_token": refresh_token_moderator,
+        },
+    )
+    assert response2.status_code == status.HTTP_200_OK
+
+    response2_data = response2.json()
+    assert "access_token" in response2_data
+    assert "refresh_token" in response2_data
